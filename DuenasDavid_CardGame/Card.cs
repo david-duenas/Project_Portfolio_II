21851afbd86303d68b344e996f14dcf75e2f6809
David Duenas <dizzyhitz@student.fullsail.edu>
2019-10-23 20:56:18 -0400

    4.0 Data Visualization Practice
    Code to shuffle cards and draw the cards as well as point totals for the cards. Connection to SQL database to pull 4 random users everytime the game is loaded.

---
+++ DuenasDavid_CardGame/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertedJSON
+{
+    class Card
+    { 
+        private string face;
+        private string suit;
+        private int points;
+        private int totalPoints;
+      
+      
+
+        public Card(string cardFace, string cardSuit, int cardPoints)
+        {
+            face = cardFace;
+            suit = cardSuit;
+            points = cardPoints;
+         
+
+        }
+     
+        
+        public static void TotalPoints()
+        {
+  
+
+                
+
+
+
+
+        }
+
+        public override string ToString()
+        {
+            return face + " of " + suit + " | " + points + " points.";
+            
+        }
+
+    }
+}
---
+++ DuenasDavid_CardGame/Deck.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertedJSON
+{
+
+    class Deck
+    {
+        public static string[] faces;
+        public static int[] points;
+
+        private Card[] deck;
+        private int currentCard;
+        private const int NumberOfCards = 52;
+        private Random randomNumber;
+
+        public Deck()
+        {
+         string[] faces = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+         int[] points = { 15, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 12, 12 };
+         
+        string[] suits = { "Hearts", "Clubs", "Spades", "Diamonds" };
+         
+       
+
+            deck = new Card[NumberOfCards];
+            currentCard = 0;
+            randomNumber = new Random();
+            string totalPoints;
+
+            for (int counter = 0; counter < deck.Length; counter++)
+                deck[counter] = new Card(faces[counter % 11], suits[counter / 13], points[counter % 11]);
+
+            
+
+
+
+            
+          
+        }
+ 
+
+            public  void Shuffle()
+            {
+                currentCard = 0;
+                for(int start = 0; start < deck.Length; start++)
+                {
+                    int end = randomNumber.Next(NumberOfCards);
+                    Card temporary = deck[start];
+                    deck[start] = deck[end];
+                    deck[end] = temporary;
+                }
+            }
+
+        public Card DealCard()
+        {
+            if (currentCard < deck.Length)
+              
+                return deck[currentCard++];
+
+            
+          
+            
+            else
+                return null;
+        }
+        
+       
+        }
+    }
+
